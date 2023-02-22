6d6dc110812d07d40a9fb8ccf9d5d9341bf8ff1e
Rapha Lobosco <83170908+raphacomics@users.noreply.github.com>
2023-02-22 21:41:34 +0000

    New Todo Vue 2

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,25 @@
+
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+    <header class="navbar bg-dark">
+        <nav class="container d-flex justifi-content-between">
+            <div class="navbar-brand text-light">
+                Rapha Todo 2.0
+            </div>
+            <div class="navbar-nav gap-3">
+                <div class="nav-item">
+                    <RouterLink class="text-light" to="/">Home</RouterLink>
+                </div>
+                <div class="nav-item">
+                    <RouterLink class="text-light" to="/about">About</RouterLink>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    Hello Home
+  </main>
+</template>
